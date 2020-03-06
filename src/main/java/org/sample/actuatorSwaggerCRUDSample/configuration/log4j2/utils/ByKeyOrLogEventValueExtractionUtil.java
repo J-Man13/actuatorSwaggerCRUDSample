0dc627e005a022f7ea84f4877a181977c65344a2
jafarjafarov <cafar.9r4@gmail.com>
2020-03-06 17:41:13 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/log4j2/utils/ByKeyOrLogEventValueExtractionUtil.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/log4j2/utils/ByKeyOrLogEventValueExtractionUtil.java
@@ -3,5 +3,5 @@ package org.sample.actuatorSwaggerCRUDSample.configuration.log4j2.utils;
 import org.apache.logging.log4j.core.LogEvent;
 
 public interface ByKeyOrLogEventValueExtractionUtil {
-    abstract String extract(LogEvent logEvent);
+    String extract(LogEvent logEvent);
 }
