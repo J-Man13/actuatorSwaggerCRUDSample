d4ea92dc75767b51f3cb6da09e4b152131235890
jafarjafarov <cafar.9r4@gmail.com>
2020-03-06 11:24:05 +0400

    Micro fix, changed exception hierarchy

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/custom/exception/MongoDocumentNotFoundException.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/custom/exception/MongoDocumentNotFoundException.java
@@ -1,20 +1,8 @@
 package org.sample.actuatorSwaggerCRUDSample.custom.exception;
 
-import org.sample.actuatorSwaggerCRUDSample.model.ErrorDesriptor;
-
-public class MongoDocumentNotFoundException extends RuntimeException{
-    private ErrorDesriptor errorDesriptor;
+public class MongoDocumentNotFoundException extends GlobalUnhandledException{
 
     public MongoDocumentNotFoundException(String description){
         super(description);
-        this.errorDesriptor = new ErrorDesriptor(this.getStackTrace()[0].getClassName(),description,this.getClass().getCanonicalName());
-    }
-
-    public ErrorDesriptor getErrorDesriptor() {
-        return errorDesriptor;
-    }
-
-    public void setErrorDesriptor(ErrorDesriptor errorDesriptor) {
-        this.errorDesriptor = errorDesriptor;
     }
 }
