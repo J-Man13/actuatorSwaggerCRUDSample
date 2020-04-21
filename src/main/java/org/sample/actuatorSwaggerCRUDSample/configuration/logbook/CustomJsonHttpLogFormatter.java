cbd9f975035493bdfef01533cf68b03146625ca8
jafarjafarov <cafar.9r4@gmail.com>
2020-04-21 14:44:37 +0400

    Setting activity id , if it was mentioned in headers

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logbook/CustomJsonHttpLogFormatter.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logbook/CustomJsonHttpLogFormatter.java
@@ -2,13 +2,16 @@ package org.sample.actuatorSwaggerCRUDSample.configuration.logbook;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.apache.logging.log4j.ThreadContext;
 import org.sample.actuatorSwaggerCRUDSample.model.BodyLogginWrapper;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.zalando.logbook.HttpMessage;
 import org.zalando.logbook.StructuredHttpLogFormatter;
 
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -29,7 +32,11 @@ public class CustomJsonHttpLogFormatter implements StructuredHttpLogFormatter {
 
     @Override
     public Optional<Object> prepareBody(final HttpMessage message) throws IOException {
-        if (message.getBodyAsString() == null)
+        List<String> activityIdList = message.getHeaders().get("activityId");
+        if (activityIdList != null && !activityIdList.isEmpty() && !StringUtils.isEmpty(activityIdList.get(0)))
+            ThreadContext.put("activity.id",activityIdList.get(0));
+
+        if (StringUtils.isEmpty(message.getBodyAsString()))
             return Optional.ofNullable(new BodyLogginWrapper(null,null));
         else {
             try { return Optional.ofNullable(new BodyLogginWrapper(message.getBodyAsString(),objectMapper.readTree(message.getBodyAsString())));} catch (IOException ioe){}
