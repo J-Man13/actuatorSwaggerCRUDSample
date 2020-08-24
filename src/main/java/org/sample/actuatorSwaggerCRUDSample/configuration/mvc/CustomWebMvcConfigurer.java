855f147958cc25cebd62808e0a63ba19f793b861
jafarjafarov <cafar.9r4@gmail.com>
2020-08-24 15:38:07 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/mvc/CustomWebMvcConfigurer.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/mvc/CustomWebMvcConfigurer.java
@@ -22,11 +22,9 @@ public class CustomWebMvcConfigurer implements WebMvcConfigurer {
         @Override
         public boolean preHandle(HttpServletRequest request,
                                  HttpServletResponse response, Object handler) throws Exception {
-            //Checking if logbook logging has been executed by extracting logbook.execution.status variable from
-            //log4j2 thread context which was defined during logbook request logging
             //Logbook request/response logging can be disabled for some path in its configuration so
-            //thread context wll not be cleared and activity.id with correlation.id will not be defined
-            //which are cleared and defined in logbook per each request
+            //Checking if activity.id and correlation.id has been set or not in the beginning of the request
+            //If they  are not set, setting them here
             if (StringUtils.isEmpty(ThreadContext.get("activity.id")) && StringUtils.isEmpty(ThreadContext.get("correlation.id"))){
                 String headerActivityId = request.getHeader("activity.id");
                 String activityId = StringUtils.isEmpty(headerActivityId)?UUID.randomUUID().toString():headerActivityId;
