979611c7441516424c7d3922acfa138188b95553
jafarjafarov <cafar.9r4@gmail.com>
2020-10-18 14:59:06 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logbook/CustomHttpRequestResponseLogWriter.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logbook/CustomHttpRequestResponseLogWriter.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.ThreadContext;
 import org.sample.actuatorSwaggerCRUDSample.configuration.logging.util.CommonLogger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.zalando.logbook.Correlation;
--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logbook/LogbookConfiguration.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logbook/LogbookConfiguration.java
@@ -1,6 +1,5 @@
 package org.sample.actuatorSwaggerCRUDSample.configuration.logbook;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.*;
