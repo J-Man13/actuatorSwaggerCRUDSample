11a0a42e0c227f5e3627b3c466cd5706ca4e69ee
jafarjafarov <cafar.9r4@gmail.com>
2020-10-19 16:09:37 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logging/util/CommonLoggingObject.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/logging/util/CommonLoggingObject.java
@@ -1,10 +1,6 @@
 package org.sample.actuatorSwaggerCRUDSample.configuration.logging.util;
 
 
-
-
-
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
