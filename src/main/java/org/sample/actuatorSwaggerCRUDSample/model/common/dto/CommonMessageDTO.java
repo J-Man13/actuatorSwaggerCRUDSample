840298c6aed70da995aca16d4211460103bd9805
jafarjafarov <cafar.9r4@gmail.com>
2020-11-04 10:48:29 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/model/common/dto/CommonMessageDTO.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/model/common/dto/CommonMessageDTO.java
@@ -1,18 +1,33 @@
 package org.sample.actuatorSwaggerCRUDSample.model.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+
 public class CommonMessageDTO {
     private String type;
     private String messageKey;
     private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime dateStamp;
+
+    public CommonMessageDTO() {
+        this.dateStamp=LocalDateTime.now();
+    }
 
     public CommonMessageDTO(String type, String messageKey, String message) {
+        this();
         this.type = type;
         this.messageKey = messageKey;
         this.message = message;
     }
 
-    public CommonMessageDTO() {
-    }
 
     public String getMessage() {
         return message;
@@ -38,4 +53,11 @@ public class CommonMessageDTO {
         this.messageKey = messageKey;
     }
 
+    public LocalDateTime getDateStamp() {
+        return dateStamp;
+    }
+
+    public void setDateStamp(LocalDateTime dateStamp) {
+        this.dateStamp = dateStamp;
+    }
 }
