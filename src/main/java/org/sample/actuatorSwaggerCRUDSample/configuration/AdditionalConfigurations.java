29d93af030f5a05cee1069646d94d20267d35f67
jafarjafarov <cafar.9r4@gmail.com>
2020-11-28 20:07:47 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/AdditionalConfigurations.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/configuration/AdditionalConfigurations.java
@@ -6,20 +6,17 @@ import org.sample.actuatorSwaggerCRUDSample.model.common.dto.CommonResponseDTO;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class AdditionalConfigurations {
 
-    private final IMultiLanguageComponent multiLanguageComponent;
-
     public AdditionalConfigurations(final @Qualifier("multiLanguageFileComponent") IMultiLanguageComponent multiLanguageComponent){
-        this.multiLanguageComponent=multiLanguageComponent;
+        GlobalCommonException.setMultiLanguageComponent(multiLanguageComponent);
     }
 
     @Bean
@@ -33,8 +30,4 @@ public class AdditionalConfigurations {
         return new CommonResponseDTO(infoBuildArchiveBaseName);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void setMultiLanguageComponentInGlobalCommonException(){
-        GlobalCommonException.setMultiLanguageComponent(multiLanguageComponent);
-    }
 }
