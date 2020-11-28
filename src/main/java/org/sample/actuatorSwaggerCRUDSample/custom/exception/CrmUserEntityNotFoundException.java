24d4266fa54d46dbbb537349f65486b31eeb8507
jafarjafarov <cafar.9r4@gmail.com>
2020-11-28 19:38:22 +0400

    Micro fix

--- src/main/java/org/sample/actuatorSwaggerCRUDSample/custom/exception/CrmUserEntityNotFoundException.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/custom/exception/CrmUserEntityNotFoundException.java
@@ -1,11 +1,19 @@
 package org.sample.actuatorSwaggerCRUDSample.custom.exception;
 
 public class CrmUserEntityNotFoundException extends GlobalCommonException {
+
+    private final String attributeValue;
+
     public CrmUserEntityNotFoundException(String messageKey,String attributeValue){
         super(
                 messageKey,
                 String.format(getMultiLanguageComponent().getMessageByKey(messageKey,"en"),attributeValue),
                 String.format(getMultiLanguageComponent().getMessageByKey(messageKey),attributeValue)
         );
+        this.attributeValue = attributeValue;
+    }
+
+    public String getAttributeValue() {
+        return attributeValue;
     }
 }
--- src/main/java/org/sample/actuatorSwaggerCRUDSample/custom/exception/MongoDocumentNotFoundException.java
+++ src/main/java/org/sample/actuatorSwaggerCRUDSample/custom/exception/MongoDocumentNotFoundException.java
@@ -2,11 +2,18 @@ package org.sample.actuatorSwaggerCRUDSample.custom.exception;
 
 
 public class MongoDocumentNotFoundException extends GlobalCommonException {
+    private final String attributeValue;
+
     public MongoDocumentNotFoundException(String messageKey,String attributeValue){
         super(
                 messageKey,
                 String.format(getMultiLanguageComponent().getMessageByKey(messageKey,"en"),attributeValue),
                 String.format(getMultiLanguageComponent().getMessageByKey(messageKey),attributeValue)
         );
+        this.attributeValue = attributeValue;
+    }
+
+    public String getAttributeValue() {
+        return attributeValue;
     }
 }
